02b1e19059f61672fc642a22f641cabcf7375e09
HaruDev <sarankhtn@gmail.com>
2024-10-16 21:20:23 +0700

    fix: fix bug image cannot display

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+function HeroSection() {
+  return (
+    <main className="container px-4 py-8 lg:py-16 mx-auto">
+      <div className="flex flex-col lg:flex-row items-center">
+        <div className="lg:w-1/3 mb-8 lg:mb-0 lg:pr-8">
+          <h1 className="text-4xl lg:text-5xl font-bold mb-4">
+            Stay <br className="hidden lg:block" />
+            Informed, <br />
+            Stay Inspired,
+          </h1>
+          <p className="text-lg text-gray-500">
+            Discover a World of Knowledge at Your Fingertips. Your Daily Dose of
+            Inspiration and Information.
+          </p>
+        </div>
+        <img
+          src="./src/assets/profile.jpeg"
+          alt="Wannasing's Images"
+          className="h-[530px] object-cover rounded-lg shadow-lg lg:w-1/3 mx-4 mb-8 lg:mb-0"
+        />
+        <div className="lg:w-1/3 lg:pl-8">
+          <h2 className="text-xl font-semibold mb-2">-Author</h2>
+          <h3 className="text-2xl font-bold mb-4">Wannasingh K.</h3>
+          <p className="text-gray-500 mb-4">
+            I&apos;m a passionate animal lover who enjoys taking care of pets
+            and shopping for anything related to them, from toys to food and
+            accessories. Exploring new places is another thing I love—whether
+            it&apos;s camping in the mountains, relaxing on the beach, or
+            discovering hidden spots in nature.
+            <br />
+            <br />
+            Through my blog, I&apos;ll be sharing my adventures, experiences
+            with animals, and all the fun things I find along the way. Join me
+            on this journey and let&apos;s explore the world together!
+          </p>
+          <p className="text-gray-500">Follow me on Instagram: @oncenasingh</p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default HeroSection
