0aace715caf5054b0e3684118b4599ba3e2ffa85
Wannasingh <sarankhtn@gmail.com>
2024-11-08 16:52:00 +0700

    feat: Add not found page

--- src/pages/NotFoundpage.jsx
+++ src/pages/NotFoundpage.jsx
@@ -2,6 +2,7 @@ import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
 import { CircleAlert } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+
 export default function NotFoundPage() {
   const navigate = useNavigate();
   return (
