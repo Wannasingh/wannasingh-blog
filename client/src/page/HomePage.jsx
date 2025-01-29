6f74d6cf420ec4257bf89732f18e35e95efc7d3e
Wannasingh <sarankhtn@gmail.com>
2025-01-30 00:40:57 +0700

    feat: Add icon and change the title

--- client/src/page/HomePage.jsx
+++ client/src/page/HomePage.jsx
@@ -1,9 +1,14 @@
 import { NavBar, HeroSection, Footer } from "@/components/WebSection";
 import ArticlesSection from "@/components/ArticlesSection";
+import { Helmet } from 'react-helmet-async';
 
 export default function HomePage() {
   return (
     <div className="flex flex-col min-h-screen">
+      <Helmet>
+        <title>Wannasin Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Helmet>
       <NavBar />
       <div className="flex-grow">
         <HeroSection />
